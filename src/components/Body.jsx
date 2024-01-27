1138025733f9804853d95368b80143f466f2db58
datta934 <official.mauaa@gmail.com>
2024-01-27 22:30:23 +0530

    Initial commit

---
+++ src/components/Body.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Card from "./Card";
+
+const Body = ({cardName}) => {
+  return <Card cardName={cardName} />;
+};
+
+export default Body;
---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Card = ({cardName}) => {
+  return (
+    <div>Card {cardName}</div>
+  )
+}
+
+export default Card
